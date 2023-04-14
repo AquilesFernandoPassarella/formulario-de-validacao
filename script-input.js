b2834a4f7b26c10c672eaf1ad8bd61a2c29d21bc
Aquiles Fernando Passarella <aquilesfernandop@hotmail.com>
2023-04-13 23:52:19 -0300

    Adicionando mascara

---
+++ script-input.js
@@ -0,0 +1,39 @@
+const Cpf = document.querySelector("#cpf");
+
+Cpf.addEventListener("keypress", () => {
+        let Cpflength = Cpf.value.length
+
+        if (Cpflength === 3 || Cpflength === 7 ) {
+            Cpf.value += "."
+        }
+
+        if (Cpflength === 11){
+
+            Cpf.value += "-"
+        }
+      
+})
+
+const tel = document.querySelector("#tel");
+
+tel.addEventListener("keypress", function(e){
+    let tellenght = tel.value.length
+
+    if (tellenght === 0 )  {
+        tel.value += "("
+    }
+
+    if (tellenght === 3){
+        tel.value += ")"
+    }
+
+    if (tellenght === 3){
+        tel.value += " "
+    }
+    if (tellenght === 6){
+        tel.value += " "
+    }
+    if (tellenght === 11){
+        tel.value += "-"
+    }
+})

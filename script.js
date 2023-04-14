06a175d1786d48fbbb6cc8ae31bac679cbc35b55
Aquiles Fernando Passarella <aquilesfernandop@hotmail.com>
2023-04-14 00:37:11 -0300

    Adicionando regras aos inputs

--- script.js
+++ script.js
@@ -1,11 +1,56 @@
 const inputName = document.querySelector("#name");
 
-inputName.addEventListener("keypress", function(e){
+inputName.addEventListener("keypress", function (e) {
+  const keyCode = e.keyCode ? e.keyCode : e.wich;
 
-    const keyCode = (e.keyCode ? e.keyCode : e.wich)
+  if (keyCode > 47 && keyCode < 58) {
+    e.preventDefault();
+  }
+});
 
-    if (keyCode > 47 && keyCode <58){
-        e.preventDefault();
-    }
-})
 
+
+
+
+
+const telInput = document.querySelector("#tel");
+
+telInput.addEventListener("keypress", function (e) {
+  if (!checkChar(e)) {
+    e.preventDefault();
+  }
+});
+
+function checkChar(e) {
+  const char = String.fromCharCode(e.keyCode);
+
+  const pattern = "[0-9]";
+
+  if (char.match(pattern)) {
+    return true;
+  }
+}
+
+
+
+
+
+
+
+const cpf = document.querySelector("#cpf");
+
+cpf.addEventListener("keypress", function (e) {
+  if (!checkChar(e)) {
+    e.preventDefault();
+  }
+});
+
+function checkChar(e) {
+  const char = String.fromCharCode(e.keyCode);
+
+  const pattern = "[0-9]";
+
+  if (char.match(pattern)) {
+    return true;
+  }
+}
